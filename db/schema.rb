a75dd72e2109e64e05255b50e01fb9133c29e5f6
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-05-30 15:38:41 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_193807) do
+ActiveRecord::Schema.define(version: 2022_05_30_203530) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
